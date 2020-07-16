84f8f2f7157861c0a07c5b8ea0827b671d04144c
Fabian Höring <f.horing@criteo.com>
2020-07-16 14:30:57 +0200

    Add setup.py files

---
+++ setup.py
@@ -0,0 +1,57 @@
+import os
+import setuptools
+import versioneer
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+DESCRIPTION = ("")
+
+try:
+    LONG_DESCRIPTION = open(os.path.join(here, "README.md"), encoding="utf-8").read()
+except Exception:
+    LONG_DESCRIPTION = ""
+
+
+def _read_reqs(relpath):
+    fullpath = os.path.join(os.path.dirname(__file__), relpath)
+    with open(fullpath) as f:
+        return [s.strip() for s in f.readlines()
+                if (s.strip() and not s.startswith("#"))]
+
+
+REQUIREMENTS = _read_reqs("requirements.txt")
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Operating System :: POSIX :: Linux",
+    "Environment :: Console",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Software Development :: Libraries"
+]
+
+
+setuptools.setup(
+    name="mlflow-yarn",
+    packages=setuptools.find_packages(),
+    version=versioneer.get_version(),
+    install_requires=REQUIREMENTS,
+    tests_require=["pytest"],
+    python_requires=">=3.6",
+    maintainer="Criteo",
+    maintainer_email="github@criteo.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    classifiers=CLASSIFIERS,
+    keywords="mlflow",
+    url="https://github.com/criteo/mlflow-yarn",
+    entry_points={
+        "mlflow.project_backend":
+            "yarn=mlflow_yarn.yarn_backend:YarnProjectBackend",
+    },
+)
