e3199232502014e835d754fb2ef1f5cf74b88b0a
Fabian Höring <f.horing@criteo.com>
2020-07-31 11:16:20 +0200

    Use cluster-pack master branch

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -7,7 +7,6 @@ pushd mlflow-yarn
     python3.6 -m venv ~/venv
     source ~/venv/bin/activate
     pip install -U pip setuptools
-    pip install cluster-pack@git+https://github.com/fhoering/cluster-pack.git@build_from_spec
     pip install .
     pip install pytest
     pytest -m integration -s tests --log-cli-level=INFO
